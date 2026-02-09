149229a9476f959697cc462ceb3025400c0fbedc
RudraRaushan01 <rudraraushan000@gmail.com>
2026-02-09 22:47:41 +0530

    s commit

---
+++ LinkedHashSet/LinkHash1.java
@@ -0,0 +1,13 @@
+package LinkedHashSet;
+import java.util.*;
+public class LinkHash1 {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> lhs=new LinkedHashSet<>();
+        lhs.add(78);
+        lhs.add(65);
+        lhs.add(38);
+        lhs.add(49);
+        lhs.add(18);
+        System.out.println(lhs);
+    }
+}
---
+++ TreeSet/LinkHash1.java
@@ -0,0 +1,13 @@
+package Treeset;
+import java.util.*;
+public class LinkHash1 {
+    public static void main(String[] args) {
+        TreeSet<Integer> lhs=new TreeSet<>();
+        lhs.add(78);
+        lhs.add(65);
+        lhs.add(38);
+        lhs.add(49);
+        lhs.add(18);
+        System.out.println(lhs);
+    }
+}
