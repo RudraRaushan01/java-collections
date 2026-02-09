d20c8c922b38b6ad768a94e2a86bbcc534610a58
RudraRaushan01 <rudraraushan000@gmail.com>
2026-02-09 22:49:48 +0530

    Add files via upload

---
+++ Arraylist/Basic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Basic{
+    public static void main(String args[]){
+        ArrayList al=new ArrayList<>();
+        al.add(34);
+        al.add(65);
+        al.add(87);
+        al.add(74);
+        al.add(90);
+        al.add(19);
+        System.out.println(al);
+    }
+}
---
+++ Hashset/Hashing1.java
@@ -0,0 +1,13 @@
+package Hashset;
+import java.util.*;
+class Hashing{
+    public static void main(String[] args) {
+        HashSet<Integer> h=new HashSet<>();
+        h.add(16);
+        h.add(32);
+        h.add(48);
+        h.add(64);
+        h.add(80);
+        System.out.println(h);
+    }
+}
